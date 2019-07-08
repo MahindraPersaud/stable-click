87ee9c0eeffe9353c1a67851201229e5decef2c2
mahindra <mp3494@nyu.edu>
2019-07-08 04:43:51 -0400

    Comments for add, delete, and communication

--- stable_click/k8Communication.sh
+++ stable_click/k8Communication.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Connecting Kubernetes master to worker nodes
+# * Outputs the config file for kubeconfig
+# * Outputs the config_map for aws authentication
+# * Applys the config_map to the cluster
+#
+
 terraform output kubeconfig > ~/.kube/config
 terraform output config_map_aws_auth > config-map-aws-auth.yaml
 kubectl apply -f config-map-aws-auth.yaml
